f95a2a6838e18bfcef93ce026dc93d792b322f67
Replit user <>
2023-07-12 16:39:28 -0400

    beta-0.0.19

---
+++ target/release/deps/lazy_static-8f4bbb1305910a7e.d
@@ -0,0 +1,8 @@
+C:\Users\Administrator\Documents\decalang\target\release\deps\lazy_static-8f4bbb1305910a7e.rmeta: C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\lazy_static-1.4.0\src\lib.rs C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\lazy_static-1.4.0\src\inline_lazy.rs
+
+C:\Users\Administrator\Documents\decalang\target\release\deps\liblazy_static-8f4bbb1305910a7e.rlib: C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\lazy_static-1.4.0\src\lib.rs C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\lazy_static-1.4.0\src\inline_lazy.rs
+
+C:\Users\Administrator\Documents\decalang\target\release\deps\lazy_static-8f4bbb1305910a7e.d: C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\lazy_static-1.4.0\src\lib.rs C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\lazy_static-1.4.0\src\inline_lazy.rs
+
+C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\lazy_static-1.4.0\src\lib.rs:
+C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\lazy_static-1.4.0\src\inline_lazy.rs:
---
+++ target/release/deps/windows_x86_64_gnu-0e4ae02aa74e77bd.d
@@ -0,0 +1,7 @@
+C:\Users\Administrator\Documents\decalang\target\release\deps\windows_x86_64_gnu-0e4ae02aa74e77bd.rmeta: C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_gnu-0.48.0\src\lib.rs
+
+C:\Users\Administrator\Documents\decalang\target\release\deps\libwindows_x86_64_gnu-0e4ae02aa74e77bd.rlib: C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_gnu-0.48.0\src\lib.rs
+
+C:\Users\Administrator\Documents\decalang\target\release\deps\windows_x86_64_gnu-0e4ae02aa74e77bd.d: C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_gnu-0.48.0\src\lib.rs
+
+C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_gnu-0.48.0\src\lib.rs:
